7061067c7d34c403cad8c76cb1b8c7adaa10d6c5
LovreCikojevic120 <56341053+LovreCikojevic120@users.noreply.github.com>
2023-01-09 16:45:07 +0100

    Added login modal

--- cistoca_frontend/components/Header.js
+++ cistoca_frontend/components/Header.js
@@ -1,25 +1,27 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import { useState } from 'react';
 import logo from '../public/images/cistoca_logo.svg';
+import LoginModal from './LoginModal';
 
 const Header = () => {
   return(
 	<div className='flex flex-row justify-between'>
 		<Image className='ml-10' src={logo} width='470px' height='105px'/>
 		<div className='flex flex-row justify-evenly bg-[#9DEC8F] rounded-bl-full'>
-			<NavbarLink linkName={'Usluge'}></NavbarLink>
-			<NavbarLink linkName={'Edukacija'}></NavbarLink>
-			<NavbarLink linkName={'Informacije'}></NavbarLink>
-			<button className='w-24'>Prijavi se</button>
+			<NavbarLink path={'/servicePage'} linkName={'Usluge'}></NavbarLink>
+			<NavbarLink path={'/'} linkName={'Edukacija'}></NavbarLink>
+			<NavbarLink path={'/'} linkName={'Informacije'}></NavbarLink>
+			<LoginModal/>
 		</div>
 	</div>
   )
 }
 
-const NavbarLink = ({linkName}) => {
+const NavbarLink = ({path, linkName}) => {
 	return(
 	<div className="m-16 relative group">
-		<Link href='/'>{linkName}</Link>
+		<Link href={path}>{linkName}</Link>
 		<span className="absolute -bottom-1 left-0 w-0 h-1 bg-[#1D7110] transition-all group-hover:w-full"></span>
 	</div>
 	)
---
+++ cistoca_frontend/pages/servicePage.js
@@ -0,0 +1,14 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+const servicePage = () => {
+  return(
+    <>
+    <Header/>
+    <h1>AAAAAAAAAAAAAAAAAAa</h1>
+    <Footer/>
+    </>
+  )
+}
+
+export default servicePage;
