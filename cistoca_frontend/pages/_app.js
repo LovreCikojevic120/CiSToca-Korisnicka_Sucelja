ee48aae7a10cac5f588e9b3aa3ad8e3432f1fab0
mmaret00 <mmaret00@fesb.hr>
2023-02-13 16:06:34 +0100

    Add education frontpage

--- cistoca_frontend/pages/_app.js
+++ cistoca_frontend/pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import '../styles/frontPage.css'
 import '../styles/footer.css'
 import '../styles/404.css'
+import '../styles/education.css'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
--- cistoca_frontend/pages/educationPage.js
+++ cistoca_frontend/pages/educationPage.js
@@ -1,61 +1,46 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Link from "next/link";
+import Image from "next/image"
+import image1 from '../public/education1.jpg'
+import image2 from '../public/education2.jpg'
+import image3 from '../public/education3.jpg'
 
 const educationPage = () => {
   return(
     <>
     <Header/>
-    <div>
-      Odlagališta pretrpana smećem postala su nažalost naša svakodnevica i veliki problem. No, to ne mora biti naša sudbina.
+    <h1 className="font-bold text-8xl text-[#1D7110] education-title">Edukacija</h1>
 
-      Razumnim postupanjem s otpadom, najveći dio bačenih ostataka (s)tvari može se preraditi i pretvoriti u nove vrijedne i korisne tvari.
+    <div className="education-article--wrapper">
 
-      Bezuvjetna pretpostavka za takvo civilizirano postupanje s otpadom jest odvojeno sakupljanje. Odvajanjem po vrstama otpada već u kućanstvu postiže se bolja kvaliteta sekundarnih sirovina i lakša prerada.
+      <div className="education-article">
+        <Link href="/">
+          <Image src={image1} className='px-8 py-5 education-article--image'></Image>
+        <h1 class="education-article--title">Što je otpad?</h1>
+        </Link>
+        <p class="education-article--text">Odlagališta pretrpana smećem postala su nažalost naša svakodnevica i veliki problem. No, to ne mora biti naša sudbina. Razumnim postupanjem s otpadom, najveći dio bačenih ostataka (s)tvari može se preraditi i pretvoriti u nove vrijedne i korisne tvari.</p>
+        <Link  className="education-article--link" href='/'>⮞ Više</Link>
+      </div>
 
-      Stvaranje otpada često ne možemo spriječiti. No, ako s njim odgovorno postupamo, otpad ne mora nužno biti zlo.
+      <div className="education-article">
+        <Link href="/">
+          <Image src={image2} className='px-8 py-5 education-article--image'></Image>
+        <h1 class="education-article--title">Organski otpad - smeće ili korisno dobro?</h1>
+        </Link>
+        <p class="education-article--text">Kada bismo sve otpatke u našoj posudi za smeće razvrstali i odvojeno složili, ustanovili bismo da organski otpad čini čak oko 30% ukupnog otpada u kućanstvu. U organski otpad ubrajamo sav biorazgradivi otpad, npr. ostatke voća i povrća, ljuske jaja, talog kave, ostatke čaja...</p>
+        <Link  className="education-article--link" href='/'>⮞ Više</Link>
+      </div>
 
-      KAKO IZBJEĆI STVARANJE OTPADA?
-      Ako imate vrt, organski otpad (zeleni otpad iz vrta i kuhinje) ne bacajte u kantu, nego ga kompostirajte.
-      Namirnice kupujte po mogućnosti svježe i bez ambalaže.
-      Koristite platnenu vrećicu, košaru ili mrežu, a ponuđenu plastičnu vrećicu ljubazno odbijte.
-      Napitke (mineralnu vodu, sokove, osvježavajuća pića, pivo, vino....) ku kupujte u povratnoj staklenoj ambalaži.
-      Izbjegavajte proizvode zapakirane u malim obrocima. Njihova ambalaža (metalna, plastična) koju bacamo često je vrijednija od samog proizvoda
-      Ne služite se niti u iznimnim prilikama (rođendani, zabave) plastičnim priborom za jelo, posuđem i čašama za jednokratnu uporabu.
-      Koristite baterije (pisaljke, upaljače...) koji se mogu ponovno puniti.
-      Koristite platnene maramice i ubruse. Ako kupujete papirnate, birajte proizvode od recikliranog papira. To vrijedi i za sve ostale papirnate proizvode.
-      Izbjegavajte kupovanje jeftinih (plastičnih) proizvoda, posebno igračaka, jer ih zbog kratkog vijeka trajanja s pravom možemo smatrati lijepo zapakiranim smećem.
-      Nastojte kupovati proizvode dužeg vijeka trajanja kao i one koje u slučaju kvara ne morate baciti nego popraviti
-      Rabljenoj odjeći i obući nije mjesto u kanti za smeće. Raspitajte se kod Crvenog križa, Caritasa ili drugih ustanova koje skrbe o potrebitima preuzimaju li rabljenu odjeću i obuću.
-      Rabljene, dobro očuvane predmete ustupite ili prodajte (besplatnim oglašavanjem).
+      <div className="education-article">
+        <Link href="/">
+          <Image src={image3} className='px-8 py-5 education-article--image'></Image>
+        <h1 class="education-article--title">Edukacija djece</h1>
+        </Link>
+        <p class="education-article--text">Jedan od najvećih problema današnjice je problem zbrinjavanja otpada, sve veće količine otpada, koje iz godine u godinu sve više rastu. Nove tehnologije koje reproduciraju nove vrste otpada. Problemi sa deponijima, zagađivanjem okoliša, troškovima održavanja, saniranja...</p>
+        <Link className="education-article--link" href='/'>⮞ Više</Link>
+      </div>
 
-      ODVOJENO PRIKUPLJANJE PO VRSTAMA
-      ODVOJENIM PRIKUPLJNJEM SMANJUJEMO VOLUMEN OTPADA NA ODLAGALIŠTIMA, ŠTEDIMO VODU I ENERGIJU.
-      Organski otpad čini nešto više od jedne trećine ukupnog otpada u kanti za smeće, a sastoji se od kuhinjskih otpadaka (ostaci voća i povrća, ljuske jaja, ostaci kave i čaja, uvelo cvijeće...) otpadaka iz vrta (lišće, otpalo voće, sitno isjeckano granje, korovi, trava...) Kompostiranje je najprihvatljiviji način zbrinjavanja organskog otpada. Kompostiranjem se iz organskog otpada dobiju vrijedne humusne tvari koje služe za obnovu i poboljšanje kvalitete tla.
-
-      Papir čini drugu trećinu sadržaja kanti za smeće. Recikliranjem starog papira štedi se električna energija, voda i drvo. Za proizvodnju 1 tone papira srednje kvalitete mora se posjeći dva stabla i potrošiti 240 000 l vode i 4700 kWh električne energije. Istu količinu papira može se proizvesti od starog papira uz utrošak 180 l vode i 2750 kWh električne energije.
-
-      Plastika u kanti za smeće poseban je problem. Oporaba plastičnog otpada komplicirana je i otežana mnogovrsnošću plastičnih materijala. Zato je s ekološkog stajališta najodgovornije izbjegabvati korištenje plastičnih proizvoda, naročito plastične ambalaže. Plastična vrećica ili boca u prirodi se razgrađuje 100-1000 godina. Stoga plastičnu ambalažu koju ne možemo izbjeći moramo svakako odvojeno prikupljati.
-
-      Staklo u kanti za otpatke vrijedna je sekundarna sirovina. Odvojenim prikupljanjem po vrstama stakla i recikliranjem štedi se sirovine i energiju. Jedna povratna staklena boca zamjenjuje 40 nepovratnih, zo znači - 40 puta manji volumen otpada!
-
-      Metali se u mnogim gradovima i mjestima otkupljuju već odavno. Organiziranim odvojenim sakupljanjem bolje će se iskoristiti ove vrijedne sekundarne sirovine i spriječiti njihovo rasipanje.
-
-      Opasni otpad čini samo mali dio ukupne mase otpadaka, ali predstavlja veliku opasnost za okoliš. Tu spadaju: ostaci lijekova, baterije i akumulatori, rabljeno motorno ulje, pesticidi, herbicidi, ostaci boja i lakova, elektronički otpad...
-
-      Ostali otpad čini građevinski materijal, koža, auto-gume...On se također velikim dijelom može reciklirati.
-
-      NA KAREPOVAC SVAKOG DANA BACIMO 350 do 390 tona otpada,od toga:
-
-      104 tone organskog otpada iz kuhinje, vrtova i parkova
-      63 tone papira i kartona
-      44 tone plastike
-      21 tonu stakla
-      10 tona metala
-      5,7 tona drva
-      12 tona pelena
-      10,8 tona tetrapaka
-      63 tone "sitnica" do 40 mm
-      3,5 tone opasnog otpada
     </div>
     <Footer/>
     </>
