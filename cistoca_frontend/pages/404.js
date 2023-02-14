3725a006e5f3ce2e294a4fb9816e984571455675
mmaret00 <mmaret00@fesb.hr>
2023-02-14 18:35:20 +0100

    Responsiveness - 404

--- cistoca_frontend/pages/404.js
+++ cistoca_frontend/pages/404.js
@@ -8,7 +8,7 @@ const NotFoundPage = () => {
             <Header/>
             <div className="w-[90%] background-404 absolute bg-[#9DEC8F] -z-10 rounded-[10px]">
                 <div className="title-404">
-                    <h1 className="font-bold text-8xl">Stranica koju ste tražili</h1>
+                    <h1 className="font-bold text-8xl">Stranica koju ste tražili </h1>
                     <h1 className="font-bold text-8xl text-[#1D7110]">ne postoji</h1>
                 </div>
             </div>
