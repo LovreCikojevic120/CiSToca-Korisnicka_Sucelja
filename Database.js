e9c961641c98957d52d6fc396261613cfded7c9e
LovreCikojevic120 <56341053+LovreCikojevic120@users.noreply.github.com>
2023-02-15 18:57:41 +0100

    Image fix

--- Database.js
+++ Database.js
@@ -19,28 +19,28 @@ const database = {
       postID: 0, 
       postStreet: 'Ulica kralja Tomislava 6', 
       postDesc: 'AAA mrzin smeće',
-      postImgURL: '/../public/services1.jpg',
+      postImgURL: '/services1.jpg',
       postOwner: 'Lovre'
     },
     {
       postID: 1,
       postStreet: 'Ulica kralja Zvonimira 30', 
       postDesc: 'AAA mrzin smeće',
-      postImgURL: '/../public/services1.jpg',
+      postImgURL: '/services1.jpg',
       postOwner: 'Mislav'
     },
     {
       postID: 3,
       postStreet: 'Ulica kralja Tomislava 6', 
       postDesc: 'AAA mrzin smeće',
-      postImgURL: '/../public/services1.jpg',
+      postImgURL: '/services1.jpg',
       postOwner: 'Ivica'
     },
     {
       postID: 4,
       postStreet: 'Ulica kralja Tomislava 6', 
       postDesc: 'AAA mrzin smeće',
-      postImgURL: '/../public/services1.jpg',
+      postImgURL: '/services1.jpg',
       postOwner: 'Ivica'
     }
   ]
