576803ebcd45e15243a2170a86e23c7fd42be52d
LovreCikojevic120 <56341053+LovreCikojevic120@users.noreply.github.com>
2023-02-16 12:16:35 +0100

    Added card filter

--- pages/ocistimoGrad.js
+++ pages/ocistimoGrad.js
@@ -8,11 +8,23 @@ import Card from "../components/Card";
 const ocistimoGrad = () => {
   const [showNewPostBtn, setShowNewPostBtn] = useState(false);
   const [posts, setPosts] = useState([]);
+  const [filterPosts, setFilterPosts] = useState([]);
 
   useEffect(() => {
     const postStorage = getPostStorage();
     setPosts(postStorage);
+    setFilterPosts(postStorage);
   }, []);
+
+  const filterBySearch = (e) => {
+    const query = e.target.value;
+    let updatedList = [...posts];
+
+    updatedList = updatedList.filter(item => 
+      item.postStreet.toLowerCase().indexOf(query.toLowerCase()) !== -1);
+
+    setFilterPosts(updatedList);
+  }
 
   return(
     <>
@@ -25,9 +37,15 @@ const ocistimoGrad = () => {
     <p className="education-subpage--text ocistimo-subtitle">Klikom na zelenu tipku "Nova objava", koja se nalazi dolje desno, imate mogućnost korištenja sustava. Za korištenje sustava je potrebna prijava. Stavite fotografiju smeća u gradu, te unesite ulicu kojoj je fotografija slikana i opis, a mi ćemo se pobrinuti za čišćenje!</p>
     
     <div className="ocistimo-wrapper">
-      <NewPostModal showBtn={showNewPostBtn} setPostArray={setPosts} postArray={posts}/>
+      <div className="flex flex-row items-center justify-between">
+        <div className="ml-10">
+          <div className="odvoz--search-text">Unesite ulicu za filtriranje:&nbsp;</div>
+          <input id='odvoz--search-box' placeholder="&nbsp;Ime ulice" onChange={(e) => filterBySearch(e)} />
+        </div>
+        <NewPostModal showBtn={showNewPostBtn} setPostArray={setPosts} postArray={posts}/>
+      </div>
       <div className="card-holder">
-        {posts && posts.map((post) => <Card post={post} setPost={setPosts} key={post.postID}/>)}
+        {filterPosts && filterPosts.map((post) => <Card post={post} setPost={setPosts} key={post.postID}/>)}
       </div>
     </div>
     <Footer/>
