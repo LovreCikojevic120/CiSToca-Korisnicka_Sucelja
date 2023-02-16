912199aab694205479c2de0b41354869ade431f8
mmaret00 <mmaret00@fesb.hr>
2023-02-16 11:42:18 +0100

    Remove unnecessary lines

--- pages/serviceOdvoz.js
+++ pages/serviceOdvoz.js
@@ -2,7 +2,6 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Link from "next/link";
 import { useState } from "react";
-import { GiMagnifyingGlass } from 'react-icons/fa';
 
 
 const serviceOdvoz = () => {
